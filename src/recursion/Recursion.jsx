e256790dd6ae00656c2a5f1806d09a8de0173854
blazej-bryla <brykuski@gmail.com>
2023-05-15 15:31:57 +0200

    create 3 components in recursion.jsx

--- src/recursion/Recursion.jsx
+++ src/recursion/Recursion.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import RecursiveComponent from './RecursiveComponent';
-import useTimer from '../hooks/useTimer';
+import React from "react";
+import RecursiveComponent from "./RecursiveComponent";
+import useTimer from "../hooks/useTimer";
 
-// write components here
-const One = () => null;
-const Two = () => null;
-const Three = () => null;
+const One = ({ children }) => {
+  <div className="App-box">
+    One
+    {children}
+  </div>;
+};
+const Two = ({ children }) => {
+  <div className="App-box">
+    Two
+    {children}
+  </div>;
+};
+const Three = ({ children }) => {
+  <div className="App-box">
+    Three
+    {children}
+  </div>;
+};
 
-const components = [
-	One,
-	Two,
-	Three,
-];
+const components = [One, Two, Three];
 
 function Recursion() {
-	const seconds = useTimer();
+  const seconds = useTimer();
 
-	return (
-		<div className='App-wrapper'>
-			<div className='App-timer'>
-				{seconds} seconds
-			</div>
-			<RecursiveComponent components={components} />
-		</div>
-	);
+  return (
+    <div className="App-wrapper">
+      <div className="App-timer">{seconds} seconds</div>
+      <RecursiveComponent components={components} />
+    </div>
+  );
 }
 
 Recursion.propTypes = {
